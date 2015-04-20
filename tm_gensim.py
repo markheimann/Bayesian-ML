6fa51cf8ae68d760abc6f62a27728d45c4e2a425
= <mheimann@umich.edu>
2015-04-20 10:56:27 -0500

    Tweaked Python LDA/NMF.  Added example of LDA in Gensim. Roadmap of using LDA for document classification (also see Google doc)

---
+++ tm_gensim.py
@@ -0,0 +1,84 @@
+#https://gist.github.com/aronwc/8248457
+import numpy as np
+ 
+from gensim import matutils
+from gensim.models.ldamodel import LdaModel
+from sklearn import linear_model
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer
+ 
+def print_features(clf, vocab, n=10):
+   """ Print sorted list of non-zero features/weights. """
+   coef = clf.coef_[0]
+   print 'positive features: %s' % (' '.join(['%s/%.2f' % (vocab[j], coef[j]) for j in np.argsort(coef)[::-1][:n] if coef[j] > 0]))
+   print 'negative features: %s' % (' '.join(['%s/%.2f' % (vocab[j], coef[j]) for j in np.argsort(coef)[:n] if coef[j] < 0]))
+ 
+ 
+def fit_classifier(X, y, C=0.1):
+   """ Fit L1 Logistic Regression classifier. """
+   # Smaller C means fewer features selected.
+   clf = linear_model.LogisticRegression(penalty='l1', C=C)
+   clf.fit(X, y)
+   return clf
+
+#Use classifier to make predictions on data
+#Can return those predictions and compare to true labels to calculate error''' 
+def make_predictions(clf,test_data):
+   return clf.predict(test_data)
+
+#Compute and return classification error by comparing predictions to labels 
+def compute_error(predictions,labels):
+   comp_preds = predictions - labels
+   errorRate = np.count_nonzero(comp_preds)*1.0/np.size(comp_preds)
+   return errorRate 
+
+ 
+def fit_lda(X, vocab, num_topics=5, passes=20):
+   """ Fit LDA from a scipy CSR matrix (X). """
+   print 'fitting lda...'
+   return LdaModel(matutils.Sparse2Corpus(X), num_topics=num_topics,
+                    passes=passes,
+                    id2word=dict([(i, s) for i, s in enumerate(vocab)]))
+ 
+ 
+def print_topics(lda, vocab, n=10):
+   """ Print the top words for each topic. """
+   topics = lda.show_topics(num_topics=n, formatted=False)
+   for ti, topic in enumerate(topics):
+      print 'topic %d: %s' % (ti, ' '.join('%s/%.2f' % (t[1], t[0]) for t in topic))
+ 
+ 
+if (__name__ == '__main__'):
+   # Load data.
+   #allows you to specify which categories you want
+   #shuffles the data so it's in random order
+   rand = np.random.mtrand.RandomState(8675309)
+   cats = ['rec.sport.baseball', 'sci.crypt']
+   all_data = fetch_20newsgroups(categories=cats,shuffle=True,random_state=rand, remove=('headers', 'footers', 'quotes'))
+   vec = CountVectorizer(min_df=10, stop_words='english')
+   tdm_all = vec.fit_transform(all_data.data)
+   vocab = vec.get_feature_names()
+   
+   #Topic modeling 
+   # Fit LDA.
+   num_topics = 10
+   num_passes = 10
+   lda = fit_lda(tdm_all, vocab, num_topics, num_passes)
+
+   #print out LDA topics
+   print_topics(lda,vocab,num_topics)
+
+
+   #Classification using LDA topics as features
+   #Possibly compare to document classification using the term-document counts as features (which is much higher dimensional)?
+
+   #Split into training and test data
+
+   
+   #Fit classifier.
+
+   
+   #Test classifier
+
+
+   #Compute and display error rate
--- topic_modeling.py
+++ topic_modeling.py
@@ -1,50 +1,47 @@
-from __future__ import print_function
-from sklearn.feature_extraction.text import TfidfVectorizer
+#from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.decomposition import NMF
 import numpy as np
 import lda
 
+#Can play around with all these parameters and see what happens.  Visualize topics in cool ways.
 n_samples = 2000
 n_features = 1000
 n_topics = 10
 n_top_words = 10
 
 #dataset: 20 newsgroups
-print("Loading dataset and extracting document term features...")
-dataset = fetch_20newsgroups(shuffle=True, random_state=1,
-                             remove=('headers', 'footers', 'quotes'))
+print "Loading dataset and extracting document term features..."
+dataset = fetch_20newsgroups(shuffle=True, random_state=1, remove=('headers', 'footers', 'quotes'))
 
 #get features from dataset
-#vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, max_features=n_features,
-#                             stop_words='english')
+#vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, max_features=n_features, stop_words='english')
 vectorizer = CountVectorizer(stop_words='english')
 td_matrix = vectorizer.fit_transform(dataset.data[:n_samples])
 feature_names = vectorizer.get_feature_names()
 
-#Fit NMF
+#Perform NMF
 #for reference: http://scikit-learn.org/stable/auto_examples/applications/topics_extraction_with_nmf.html
-print("Fitting the NMF model with %d samples and %d features..."
-      % (n_samples, n_features))
+print("Performing NMF with %d samples and %d features..." % (n_samples, n_features))
 nmf = NMF(n_components=n_topics, random_state=1).fit(td_matrix)
 
 #print out topics
+print "NMF topics: "
 for topic_idx, topic in enumerate(nmf.components_):
     print("Topic #%d:" % topic_idx)
-    print(" ".join([feature_names[i]
-                    for i in topic.argsort()[:-n_top_words - 1:-1]]))
-    print()
+    print(" ".join([feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1]]))
+    print
 
-#Fit LDA
+#Perform LDA
 #For reference: https://pypi.python.org/pypi/lda
-print ("Fitting LDA for %d topics..." % n_topics)
+print("Performing LDA for %d topics..." % n_topics)
 model = lda.LDA(n_topics=n_topics, n_iter=200, random_state=1)
 model.fit_transform(td_matrix)  
 
 #print out topics
-topic_word = model.components_ # model.topic_word_ also works
+print "\nLDA topics: "
 for topic_idx, topic in enumerate(model.components_):
    print("Topic #%d:" % topic_idx)
    print(" ".join([feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1]]))
-   print() 
+   print 
